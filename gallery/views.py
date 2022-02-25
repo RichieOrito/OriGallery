86d1f677ee466e907e3743c712dddeefcbf48751
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-25 14:57:52 +0300

    This commit updates the index view function with global images

--- gallery/views.py
+++ gallery/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 #I define the function index which will be our view function to display our images.
+    global images
     
     return render(request, 'index.html',)
