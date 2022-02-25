c0a80f67a5913dc9b2493120068a630b5486d205
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-25 14:17:01 +0300

    This commit imports yje HttpResponse and defines the index view function for our images

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+#I define the function index which will be our view function to display our images.
+    
+    return render(request, 'index.html',)
