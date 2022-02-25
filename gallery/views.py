3a1e551649f7ef12cdc230b191ead19fe6da74ea
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-25 15:10:59 +0300

    This commit creates the location function for viewing images based on different locations

--- gallery/views.py
+++ gallery/views.py
@@ -5,5 +5,64 @@ from django.http import HttpResponse
 def index(request):
 #I define the function index which will be our view function to display our images.
     global images
-    
+
     return render(request, 'index.html',)
+
+# I define the function location which will viewing images based on various locations
+def location_nairobi(request):
+    return render(request, 'index.html')
+
+def location_kisumu(request):
+    
+    return render(request, 'index.html')
+
+def location_mombasa(request):
+    
+    return render(request, 'index.html')
+
+def location_nakuru(request):
+    
+    return render(request, 'index.html')
+
+def location_voi(request):
+    
+    return render(request, 'index.html')
+
+def location_machakos(request):
+    
+    return render(request, 'index.html')
+
+def location_taita(request):
+    
+    return render(request, 'index.html')
+
+def location_meru(request):
+    
+    return render(request, 'index.html')
+
+def location_garisa(request):
+    
+    return render(request, 'index.html')
+
+def location_kakamega(request):
+    
+    return render(request, 'index.html')
+
+def location_malindi(request):
+    
+    return render(request, 'index.html')
+
+def location_tsavo(request):
+    
+    return render(request, 'index.html')
+
+def location_nanyuki(request):
+    
+    return render(request, 'index.html')
+
+def location_nyeri(request):
+    
+    return render(request, 'index.html')
+
+
+    
