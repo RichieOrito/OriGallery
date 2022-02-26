ab7474ca114e521d304c18b90713ad9f4993018d
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-27 02:02:13 +0300

    This commit adds an image dictionary to all functions

--- gallery/views.py
+++ gallery/views.py
@@ -7,63 +7,63 @@ def index(request):
 #I define the function index which will be our view function to display our images.
     global images
     
-    return render(request, 'index.html',)
+    return render(request, 'index.html', {'images':images,})
 
 # I define the function location which will viewing images based on various locations
 def location_nairobi(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_kisumu(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_mombasa(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_nakuru(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_voi(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_machakos(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_taita(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_meru(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_garisa(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_kakamega(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_malindi(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_tsavo(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_nanyuki(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def location_nyeri(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'images':images,})
 
 def search_results(request):
 
