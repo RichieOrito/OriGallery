6fdd20a8329c368901f4ed77832b356b50cb2424
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-27 01:58:55 +0300

    This commit adds an empty list to push images

--- gallery/views.py
+++ gallery/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+images = []
 def index(request):
 #I define the function index which will be our view function to display our images.
     global images
-
+    
     return render(request, 'index.html',)
 
 # I define the function location which will viewing images based on various locations
