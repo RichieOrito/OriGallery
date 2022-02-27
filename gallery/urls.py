4d81b9718707b80a2d0b113159fdfc3b388def47
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-27 16:53:27 +0300

    This commit adds dynamic url to allow users to click different counties

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,4 +7,19 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('nairobi/', views.location_nairobi, name='location_nairobi'),
+    path('kisumu/', views.location_kisumu, name='location_kisumu'),
+    path('mombasa/', views.location_mombasa, name='location_mombasa'),
+    path('nakuru/', views.location_nakuru, name='location_nakuru'),
+    path('voi/', views.location_voi, name='location_voi'),
+    path('machakos/', views.location_machakos, name='location_machakos'),
+    path('taita/', views.location_taita, name='location_taita'),
+    path('meru/', views.location_meru, name='location_meru'),
+    path('garisa/', views.location_garisa, name='location_garisa'),
+    path('kakamega/', views.location_kakamega, name='location_kakamega'),
+    path('malindi/', views.location_malindi, name='location_malindi'),
+    path('tsavo/', views.location_tsavo, name='location_tsavo'),
+    path('nanyuki/', views.location_nanyuki, name='location_nanyuki'),
+    path('nyeri/', views.location_nyeri, name='location_nyeri'), 
+    path('search/', views.search_results, name='search_results'), 
 ]
