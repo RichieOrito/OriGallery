2b7533289fe3fcdbad166619e9540c353c826ade
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-27 23:10:04 +0300

    This commit adds the methods to all the class functions

--- gallery/models.py
+++ gallery/models.py
@@ -8,11 +8,52 @@ class Location(models.Model):
     '''
     name = models.CharField(max_length=30)
 
+    def save_location(self):
+        '''
+        Method to save the location to the database.
+        '''
+        self.save()
+
+    def delete_location(self):
+        '''
+        Method to delete the location from the database.
+        '''
+        self.delete()
+
+    @classmethod
+    def update_location(cls, id, name):
+        '''
+        Method to update the location in the database.
+        '''
+        return cls.objects.filter(id=id).update(name=name)
+
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     '''
     Class to define the category of an image on the gallery.
     '''
     name = models.CharField(max_length=30)
+
+    def save_category(self):
+        '''Method to save the category to the database.
+        '''
+        self.save()
+
+    def delete_category(self):
+        '''Method to delete the category from the database.
+        '''
+        self.delete()
+
+    @classmethod
+    def update_category(cls, id, name):
+        '''Method to update the category in the database.
+        '''
+        return cls.objects.filter(id=id).update(name=name)
+
+    def __str__(self):
+        return self.name
 
 class Image(models.Model):
     '''
@@ -25,3 +66,47 @@ class Image(models.Model):
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     category = models.ManyToManyField(Category)
     post_date = models.DateField(auto_now_add=True)
+
+    def save_image(self):
+        '''Method to save the image to the database.
+        '''
+        self.save()
+
+    def delete_image(self):
+        '''Method to delete the image from the database.
+        '''
+        self.delete()
+
+    @classmethod
+    def update_image(cls, id, image):
+        '''Method to update the image in the database.
+        '''
+        return cls.objects.filter(id=id).update(image=image)
+        
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        '''Allows us to get an image using its ID.
+        '''
+        return cls.objects.filter(id=id).all()
+
+    @classmethod
+    def search_image(cls, search_term):
+        '''Allows us to search for an image using its category.
+        '''
+        try:
+            category = Category.objects.get(name__icontains=search_term)
+        except Category.DoesNotExist:
+            images = []
+            return images
+        
+        return cls.objects.filter(category=category).all()
+
+    @classmethod
+    def filter_by_location(cls, location):
+        '''Allows us to filter images by the location.
+        '''
+        return cls.objects.filter(location=location).all()
+    
+    def __str__(self):
+        return self.name
